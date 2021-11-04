85403fecb14bb73824fb0ac805dfc10e99c14fdc
Walter <walter.arias.aguirre@gmail.com>
2021-11-04 15:31:00 -0500

    cambios en la bd

--- database/bd.js
+++ database/bd.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql');
 //parametros de conexion con la BD
- const conexion = mysql.createConnection({
+/*  const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'ejemploapi'
-}); 
-/* const conexion = mysql.createConnection({
+});  */
+ const conexion = mysql.createConnection({
     host: 'us-cdbr-east-04.cleardb.com',
     user: 'bfab45c40de209',
     password: 'b4d0e035',
     database: 'heroku_8132a5337e24aa6'
-}); */
+}); 
 
 //mysql://bfab45c40de209:b4d0e035@us-cdbr-east-04.cleardb.com/heroku_8132a5337e24aa6?reconnect=true
 //cleardb-triangular-48691
--- routes.js
+++ routes.js
@@ -1,81 +1,109 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 const route = express.Router(); //instanciamos el constructor de express
-const conexion = require('./database/bd');
-route.use(express.json());  //activamos uso de json
+const conexion = require("./database/bd");
+route.use(express.json()); //activamos uso de json
 route.use(cors());
 
-
-route.get('/',(req,res)=>{
-    res.send('Bienvenido a la API: EMPRESA XYZ');
-})
-route.get('/api/articulos', (req, res) => {   
-    conexion.query('SELECT * FROM articulo', (error,datos) => {
-        if (error) {
-            throw error;
-        } else {
-            res.send(datos);
-        }
-    }); 
+route.get("/", (req, res) => {
+  res.send("Bienvenido a la API: EMPRESA XYZ");
 });
-
+route.get("/api/articulos", (req, res) => {
+  conexion.query("SELECT * FROM articulo", (error, datos) => {
+    if (error) {
+      throw error;
+    } else {
+      res.send(datos);
+    }
+  });
+});
 
 //consultar un articulo
-route.get('/api/articulos/:codigo', (req, res) => {
-    conexion.query('SELECT * FROM articulo where codigo= ?',[req.params.id] ,(error, dato)=> {
-        if (error) {
-            throw error;
-        } else {
-            // res.send(dato);
-            res.send(dato[0].descripcion);
-        }
-    });
+route.get("/api/articulos/:codigo", (req, res) => {
+  conexion.query(
+    "SELECT * FROM articulo where codigo= ?",
+    [req.params.id],
+    (error, dato) => {
+      if (error) {
+        throw error;
+      } else {
+        // res.send(dato);
+        res.send(dato[0].descripcion);
+      }
+    }
+  );
 });
- 
- //Insertar articulo
 
- route.post('/api/articulos/',(req,res)=>{
-    let data = {descripcion:req.body.descripcion, precio:req.body.precio,existencia:req.body.existencia};
-    let sql = "INSERT INTO articulo SET  ?"
-    conexion.query(sql,data,(error, resultado)=>{
-        if (error) {
-            throw error;
-        } else {           
-            res.send(resultado);
-        }
+//Insertar articulo
 
-    })
- });
+route.post("/api/articulos/", (req, res) => {
+  let data = {
+    descripcion: req.body.descripcion,
+    precio: req.body.precio,
+    existencia: req.body.existencia,
+  };
+  let sql = "INSERT INTO articulo SET  ?";
+  conexion.query(sql, data, (error, resultado) => {
+    if (error) {
+      throw error;
+    } else {
+      res.send(resultado);
+    }
+  });
+});
 
- //editar articulo
+//editar articulo
 
- route.put('/api/articulos/:codigo', (req, res)=>{
-     let codigo = req.params.codigo;
-     let descripcion = req.body.descripcion;
-     let precio = req.body.precio;
-     let existencia = req.body.existencia;
-     let sql = "UPDATE articulo SET  descripcion = ?, precio = ?, existencia = ? WHERE codigo = ?";
-     conexion.query(sql, [descripcion, precio, existencia, codigo], (error,resultado)=>{
-        if (error) {
-            throw error;
-        } else {           
-            res.send(resultado);
-        }
-     });
- });
+route.put("/api/articulos/:codigo", (req, res) => {
+  let codigo = req.params.codigo;
+  let descripcion = req.body.descripcion;
+  let precio = req.body.precio;
+  let existencia = req.body.existencia;
+  let sql =
+    "UPDATE articulo SET  descripcion = ?, precio = ?, existencia = ? WHERE codigo = ?";
+  conexion.query(
+    sql,
+    [descripcion, precio, existencia, codigo],
+    (error, resultado) => {
+      if (error) {
+        throw error;
+      } else {
+        res.send(resultado);
+      }
+    }
+  );
+});
 
+//borrar articulo
+route.delete("/api/articulos/:codigo", (req, res) => {
+  let sql = "DELETE FROM articulo where codigo = ?";
+  conexion.query(sql, [req.params.codigo], (error, resultado) => {
+    if (error) {
+      throw error;
+    } else {
+      res.send(resultado);
+    }
+  });
+});
 
- //borrar articulo
- route.delete('/api/articulos/:codigo', (req, res)=>{
-     let sql = "DELETE FROM articulo where codigo = ?";
-     conexion.query(sql, [req.params.codigo],(error,resultado)=>{
-        if (error) {
-            throw error;
-        } else {           
-            res.send(resultado);
-        }
-     })
- });
+//inserta un usuario con pwd hashing
+route.post("/api/usuario/", (req, res) => {
+  const salt = bcrypt.genSalt(10);
+  const pwdHashing = bcrypt.hash(req.body.pwd, salt)
 
+  let data = {
+    nombre: req.body.nombre,
+    pwd: pwdHashing ,
+  };
+  let sql = "INSERT INTO usuario SET  ?";
+  conexion.query(sql, data, (error, resultado) => {
+    if (error) {
+      throw error;
+    } else {
+      res.send(resultado);
+    }
+  });
+});
 
- module.exports = route;
+module.exports = route;
